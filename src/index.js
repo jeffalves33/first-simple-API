84e14d047fe60c5094ea62b48b14f13d53db5e52
Jeferson Luiz <luiz_cleidi@hotmail.com>
2020-10-27 19:24:59 -0300

    methods HTTP created

--- src/index.js
+++ src/index.js
@@ -1,20 +1,41 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 
 
+//seek information from the bach end
+app.get("/projects", (request, response) => {
+    //return response.send("Hello World");
+    //return response.json({message: 'Hello World'});
+    return response.json([
+        'Project 1',
+        'Project 2',
+    ]);
+});
 
+//create information on the back end
+app.set("/projects", (require, response) => {
+    return response.json([
+        'Project 1',
+        'Project 2',
+    ]);
+});
 
+//change back end information
+app.put("/projects/:id", (require, response) => {
+    return response.json([
+        'Project 1',
+        'Project 2',
+    ]);
+});
 
-
-
-
-
-
-
-
-
-
-
+//delete back end information
+app.delete("/projects/:id", (require, response) => {
+    return response.json([
+        'Project 1',
+        'Project 2',
+    ]);
+});
 
 
 
