603e2bd1077bc0def57858e9b618c2c94d8060c4
Jeferson Luiz <luiz_cleidi@hotmail.com>
2020-10-28 13:39:13 -0300

    completed routes

--- src/index.js
+++ src/index.js
@@ -1,47 +1,53 @@
 const { response } = require('express');
 const express = require('express');
+const {v4: uuid_v4} = require('uuid');
 const app = express();
+ 
+//the "use" serves to add some kind of functions that all the wheels
+//will go through it
+app.use(express.json());
 
+const projects = [];
 
 //seek information from the bach end
 app.get("/projects", (request, response) => {
     //return response.send("Hello World");
     //return response.json({message: 'Hello World'});
-    return response.json([
-        'Project 1',
-        'Project 2',
-    ]);
+    return response.json(projects);
 });
 
 //create information on the back end
-app.set("/projects", (require, response) => {
-    return response.json([
-        'Project 1',
-        'Project 2',
-    ]);
+app.post("/projects", (request, response) => {
+    const {title, owner} = request.query;
+    const project = { id: uuid_v4(), title, owner};
+    projects.push(project);
+    return response.json(project);
 });
 
 //change back end information
-app.put("/projects/:id", (require, response) => {
-    return response.json([
-        'Project 1',
-        'Project 2',
-    ]);
+app.put("/projects/:id", (request, response) => {
+    const { id } = request.params;
+    const {title, owner} = request.query; //
+    const projectIndex = projects.findIndex(answer => answer.id === id);
+    if(projectIndex < 0) return response.status(400).json({ error: 'Project not found.'});
+    projects[projectIndex] = { id, title, owner};
+    return response.json(projects[projectIndex]);
 });
 
 //delete back end information
-app.delete("/projects/:id", (require, response) => {
-    return response.json([
-        'Project 1',
-        'Project 2',
-    ]);
+app.delete("/projects/:id", (request, response) => {
+    const { id } = request.params;
+    const projectIndex = projects.findIndex(x => x.id === id);
+    if(projectIndex < 0) return response.status(400).json({ error: 'Project not found.'});
+    projects.splice(projectIndex, 1);
+    return response.status(204).send();
 });
 
 
 
 
-//segundo argumento, recebe uma função q roda automática q retorna uma 
-//msg quando é atualizado
+//second argument, receives a function that runs automatically that
+//returns a message when it is updated
 app.listen(3333, () => {
     console.log('Back-end started!!!');
 });
